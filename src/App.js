41da69cfd9af260f3ad426b760dcfbfdcaffaa8f
MarcinGladkowski <marcingladkowski@gmail.com>
2023-11-12 20:31:02 +0100

    Scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article className="App-article">
+        <div className="App-calculator">
+        <div className="output">0</div>
+          <div className="keys">
+            <button className="keyOperatorAdd calculatorKey">+</button>
+            <button className="keyOperatorSub calculatorKey">-</button>
+            <button className="keyOperatorMul calculatorKey">&times;</button>
+            <button className="keyOperatorDiv calculatorKey">÷</button>
+            <button className="calculatorKey">7</button>
+            <button className="calculatorKey">8</button>
+            <button className="calculatorKey">9</button>
+            <button className="calculatorKey">4</button>
+            <button className="calculatorKey">5</button>
+            <button className="calculatorKey">6</button>
+            <button className="calculatorKey">1</button>
+            <button className="calculatorKey">2</button>
+            <button className="calculatorKey">3</button>
+            <button className="calculatorKey">0</button>
+            <button className="calculatorKey">.</button>
+            <button className="calculatorKey">AC</button>
+            <button className="calculatorKey enter">=</button>
+          </div>
+        </div>
+      </article>
     </div>
   );
 }
